8a15d179b9cf6ace164185653aed11993bdb1bbd
stg2209 <121442370+stg2209@users.noreply.github.com>
2024-02-06 00:31:32 +0530

    Making custom statless widget(home)

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main()=> runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title:Text("Footy"),
-      centerTitle: true,
-      backgroundColor: Colors.teal[400],
-    ),
-    body: Center(
-      child: Text("Home to football",
-      style:TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.bold,
-        letterSpacing: 2,
-        color: Colors.purple[300],
-
-
-      ))
-    ),
-    floatingActionButton:FloatingActionButton(onPressed: () {
-      child:Text("Click");
-      backgroundColor: Colors.red[600];
-    },
-
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("Footy",
+          style: TextStyle(
+              fontFamily: 'Roboto',
+              fontSize: 25
+          ),),
+        centerTitle: true,
+        backgroundColor: Colors.teal[400],
+      ),
+      body: Center(
+          child: Text("Home to football",
+              style:TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+                fontFamily: 'Roboto',
+                color: Colors.black,
+
+
+              ))
+      ),
+      floatingActionButton:FloatingActionButton(
+        onPressed: () {},
+        child:Text("Click me",
+        style: TextStyle(
+          color: Colors.black
+        ),),
+        backgroundColor: Colors.teal[400],
+
+      ),
+    );
+  }
+}
