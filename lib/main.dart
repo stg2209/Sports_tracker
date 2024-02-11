41f1a829fd38573addcaa5da5a4fa32b0de4c828
stg2209 <121442370+stg2209@users.noreply.github.com>
2024-02-11 17:41:56 +0530

    tabview & bottombar

--- lib/main.dart
+++ lib/main.dart
@@ -2,44 +2,124 @@ import 'package:flutter/material.dart';
 
 void main()=> runApp(MaterialApp(
   home: Home(),
+  theme: ThemeData(
+    fontFamily: 'Teko',
+  ),
 ));
+
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:Text("Footy",
-          style: TextStyle(
-              fontFamily: 'Roboto',
-              fontSize: 25
-          ),),
-        centerTitle: true,
-        backgroundColor: Colors.teal[400],
-      ),
-      body: Center(
-          child: Text("Home to football",
-              style:TextStyle(
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title:Text("Footy",
+            style: TextStyle(
+                fontSize: 25
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.teal[400],
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.people),
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+              ),
+              Tab(
+                icon: Icon(Icons.table_rows_outlined),
+              ),
+            ],
+          ),
+        ),
+        body:
+        TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("Squad Stats",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Teko',
+                ),
+              ),
+            ),
+            Center(
+              child: Text("Player Stats",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Teko',
+                ),
+              ),
+            ),
+            Center(
+              child: Text("Points table",
+              style: TextStyle(
                 fontSize: 30,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1,
-                fontFamily: 'Roboto',
+                fontFamily: 'Teko',
+              ),
+              ),
+            ),
+
+          ],
+
+        ),
+
+
+
+
+
+
+        bottomNavigationBar:Bottombar(),
+        floatingActionButton:FloatingActionButton(
+          onPressed: () {},
+          child:Text("Add team",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Teko',
                 color: Colors.black,
+              fontSize: 15,
+            ),),
+          backgroundColor: Colors.teal[400],
 
-
-              ))
-      ),
-      floatingActionButton:FloatingActionButton(
-        onPressed: () {},
-        child:Text("Click me",
-        style: TextStyle(
-          color: Colors.black
-        ),),
-        backgroundColor: Colors.teal[400],
-
+        ),
       ),
     );
   }
 }
+
+class Bottombar extends StatelessWidget {
+  const Bottombar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+          backgroundColor: Colors.deepOrange,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.sports_soccer),
+          label: 'Score',
+          backgroundColor: Colors.green,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.sensor_occupied),
+          label: 'Profile',
+          backgroundColor: Colors.green,
+        ),
+
+
+      ],
+    );
+  }
+}
+
