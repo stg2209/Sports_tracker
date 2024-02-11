5a67b4f281a46ce2c3419d027a0ee5b4edc85fb3
stg2209 <121442370+stg2209@users.noreply.github.com>
2024-02-12 00:32:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      initialIndex: 1,
+      initialIndex: 0,
       length: 3,
       child: Scaffold(
         appBar: AppBar(
